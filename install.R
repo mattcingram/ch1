4fd451ce8f1fa187863de5b83f1822084446cb2a
Matt Ingram <40709229+mattcingram@users.noreply.github.com>
2020-09-23 11:21:23 -0400

    Update install.R

--- install.R
+++ install.R
@@ -8,3 +8,23 @@ install.packages(mypackages, lib="~/binder/R/packages", verbose=TRUE)
 install.packages(mypackages2, lib="~/binder/R/packages", verbose=TRUE)
 devtools::install_github('IRkernel/IRkernel', lib="~/binder/R/packages")
 IRkernel::installspec(user=TRUE,rprofile="~/binder/.Rprofile")
+
+
+
+  
+
+install.packages("tidyverse", lib="~/binder/R/packages")
+install.packages("rmarkdown", lib="~/binder/R/packages")
+install.packages("httr", lib="~/binder/R/packages")
+install.packages("shinydashboard", lib="~/binder/R/packages")
+install.packages("leaflet", lib="~/binder/R/packages")
+install.packages("repr", lib="~/binder/R/packages")
+install.packages("IRdisplay", lib="~/binder/R/packages")
+install.packages("evaluate", lib="~/binder/R/packages")
+install.packages("crayon", lib="~/binder/R/packages")
+install.packages("pbdZMQ", lib="~/binder/R/packages")
+install.packages("devtools", lib="~/binder/R/packages")
+install.packages("uuid", lib="~/binder/R/packages")
+install.packages("digest", lib="~/binder/R/packages")
+devtools::install_github('IRkernel/IRkernel', lib="~/binder/R/packages")
+IRkernel::installspec(user=TRUE,rprofile="~/binder/.Rprofile")

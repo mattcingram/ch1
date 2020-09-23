40772b5635be0af6dc2d6716cbc0da0c8fbbd6de
Matt Ingram <40709229+mattcingram@users.noreply.github.com>
2020-09-23 08:50:20 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,2 +1,10 @@
+.libPaths( c( "~/binder/R/packages", .libPaths()) )
+
+# packages from original publication
 mypackages <- c("Hmisc", "tidyverse", "GGally", "car", "interplot", "interflex", "gridExtra", "coefplot", "psych")
-install.packages(mypackages, verbose=TRUE)
+# packages for binder
+mypackages2 <- c("IRdisplay", "devtools")
+install.packages(mypackages, lib="~/binder/R/packages", verbose=TRUE)
+install.packages(mypackages2, lib="~/binder/R/packages", verbose=TRUE)
+devtools::install_github('IRkernel/IRkernel', lib="~/binder/R/packages")
+IRkernel::installspec(user=TRUE,rprofile="~/binder/.Rprofile")
